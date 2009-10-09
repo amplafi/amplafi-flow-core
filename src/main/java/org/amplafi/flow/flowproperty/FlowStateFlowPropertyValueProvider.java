fbaae4d07448992376595df313044de7932a3396
patmoore <patmoore@amplafi.com>
2009-10-09 18:57:43 +0000

    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-flow-core/trunk@434 6eaf30a2-012b-4626-ba8e-69dab18af4f2

---
+++ src/main/java/org/amplafi/flow/flowproperty/FlowStateFlowPropertyValueProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.amplafi.flow.flowproperty;
+
+import org.amplafi.flow.FlowActivity;
+import org.amplafi.flow.FlowPropertyDefinition;
+import org.amplafi.flow.FlowState;
+import org.amplafi.flow.impl.FlowStateImplementor;
+import static org.amplafi.flow.FlowConstants.*;
+
+/**
+ * FlowPropertyValueProvider that does flowState lookup.
+ * @author patmoore
+ *
+ */
+public class FlowStateFlowPropertyValueProvider extends AbstractFlowPropertyValueProvider<FlowActivity> implements FlowPropertyDefinitionProvider {
+
+    public static final FlowStateFlowPropertyValueProvider INSTANCE = new FlowStateFlowPropertyValueProvider();
+    /**
+     * @see org.amplafi.flow.flowproperty.FlowPropertyDefinitionProvider#defineFlowPropertyDefinitions(org.amplafi.flow.flowproperty.FlowPropertyProvider)
+     */
+    @Override
+    public void defineFlowPropertyDefinitions(FlowPropertyProvider flowPropertyProvider) {
+        flowPropertyProvider.addPropertyDefinitions(
+            new FlowPropertyDefinitionImpl(FSRETURN_TO_FLOW, FlowStateImplementor.class).initAccess(PropertyScope.flowLocal, PropertyUsage.io),
+            new FlowPropertyDefinitionImpl(FSCONTINUE_WITH_FLOW, FlowStateImplementor.class).initAccess(PropertyScope.flowLocal, PropertyUsage.io)
+            );
+    }
+
+    /**
+     * @see org.amplafi.flow.FlowPropertyValueProvider#get(org.amplafi.flow.FlowActivity, org.amplafi.flow.FlowPropertyDefinition)
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T get(FlowActivity flowActivity, FlowPropertyDefinition flowPropertyDefinition) {
+        FlowStateImplementor flowStateImplementor = flowActivity.getFlowState();
+        String lookupKey = flowStateImplementor.getRawProperty(flowActivity, flowPropertyDefinition);
+        FlowState flowState = flowStateImplementor.getFlowManagement().getFlowState(lookupKey);
+        return (T) flowState;
+    }
+
+}
