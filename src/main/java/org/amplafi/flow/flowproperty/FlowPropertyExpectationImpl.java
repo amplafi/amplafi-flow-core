46a414326b11382f0a5b0cdc767a851df8a4a998
patmoore <patmoore@amplafi.com>
2010-08-17 18:25:54 +0000

    FlowPropertyExpectation to help customize a FlowPropertyDefinition
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-flow-core/trunk@815 6eaf30a2-012b-4626-ba8e-69dab18af4f2

---
+++ src/main/java/org/amplafi/flow/flowproperty/FlowPropertyExpectationImpl.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.amplafi.flow.flowproperty;
+
+import java.util.List;
+
+import org.amplafi.flow.FlowActivityPhase;
+import org.amplafi.flow.FlowPropertyExpectation;
+
+/**
+ * Used to help configure properties created by {@link FlowPropertyDefinitionProvider}s
+ * @author patmoore
+ *
+ */
+public class FlowPropertyExpectationImpl implements FlowPropertyExpectation {
+
+    private final List<FlowPropertyValueChangeListener> flowPropertyValueChangeListeners;
+
+    private final String name;
+    private final FlowActivityPhase propertyRequired;
+    private final PropertyScope propertyScope;
+    private final PropertyUsage propertyUsage;
+    /**
+     * @param name
+     * @param propertyRequired
+     * @param propertyScope
+     * @param propertyUsage
+     * @param flowPropertyValueChangeListeners
+     */
+    public FlowPropertyExpectationImpl(String name, FlowActivityPhase propertyRequired, PropertyScope propertyScope, PropertyUsage propertyUsage,
+        List<FlowPropertyValueChangeListener> flowPropertyValueChangeListeners) {
+        this.name = name;
+        this.propertyRequired = propertyRequired;
+        this.propertyScope = propertyScope;
+        this.propertyUsage = propertyUsage;
+        this.flowPropertyValueChangeListeners = flowPropertyValueChangeListeners;
+    }
+    /**
+     * @return the flowPropertyValueChangeListeners
+     */
+    public List<FlowPropertyValueChangeListener> getFlowPropertyValueChangeListeners() {
+        return flowPropertyValueChangeListeners;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @return the propertyRequired
+     */
+    public FlowActivityPhase getPropertyRequired() {
+        return propertyRequired;
+    }
+    /**
+     * @return the propertyScope
+     */
+    public PropertyScope getPropertyScope() {
+        return propertyScope;
+    }
+    /**
+     * @return the propertyUsage
+     */
+    public PropertyUsage getPropertyUsage() {
+        return propertyUsage;
+    }
+}
