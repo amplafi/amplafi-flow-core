df4f398526bb496afd9f4fc9080fb715a1a21420
Patrick Moore <patmoore@amplafi.com>
2011-02-05 23:09:07 -0800

    better loction for test

--- src/test/java/org/amplafi/flow/flowproperty/TestDataClassDefinition.java
+++ src/test/java/org/amplafi/flow/flowproperty/TestDataClassDefinition.java
@@ -13,18 +13,22 @@
  */
 package org.amplafi.flow.flowproperty;
 
+import org.amplafi.flow.DataClassDefinition;
 import org.amplafi.flow.flowproperty.DataClassDefinitionImpl;
 import org.testng.annotations.Test;
 
 
 import static org.testng.Assert.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Test {@link DataClassDefinitionImpl}.
  */
 public class TestDataClassDefinition {
+    private static final boolean TEST_ENABLED = true;
     @Test
     public void testMap() {
         DataClassDefinitionImpl def = DataClassDefinitionImpl.map(Integer.class, String.class);
@@ -32,5 +36,40 @@ public class TestDataClassDefinition {
         assertEquals(def.getDataClass(), Map.class);
         assertEquals(def.getKeyDataClassDefinition().getDataClass(), Integer.class);
         assertEquals(def.getElementDataClassDefinition().getDataClass(), String.class);
+    }
+
+    /**
+     * Test {@link DataClassDefinition} for simple collections
+     */
+    @Test(enabled=TEST_ENABLED)
+    public void testDataClassDefinition() {
+        DataClassDefinitionImpl dataClassDefinition =
+            new DataClassDefinitionImpl(Boolean.class, Set.class);
+        assertEquals(dataClassDefinition.getElementDataClassDefinition().getDataClass(), Boolean.class);
+        assertEquals(dataClassDefinition.getDataClass(), Set.class);
+    }
+
+    /**
+     * Test {@link DataClassDefinition} for more complex collections (Map of lists )
+     */
+    @Test(enabled=TEST_ENABLED)
+    public void testDataClassDefinitionCollection() {
+        DataClassDefinitionImpl dataClassDefinition =
+            new DataClassDefinitionImpl(Boolean.class, Set.class);
+        assertTrue(dataClassDefinition.isCollection());
+        dataClassDefinition =
+            new DataClassDefinitionImpl(Set.class);
+        assertTrue(dataClassDefinition.isCollection());
+        assertFalse(dataClassDefinition.isMap());
+
+        dataClassDefinition =
+            new DataClassDefinitionImpl(Map.class);
+        assertTrue(dataClassDefinition.isCollection());
+        assertTrue(dataClassDefinition.isMap());
+        assertEquals(dataClassDefinition.getDataClass(), Map.class );
+        dataClassDefinition =
+            new DataClassDefinitionImpl(List.class);
+        assertTrue(dataClassDefinition.isCollection());
+        assertFalse(dataClassDefinition.isMap());
     }
 }
