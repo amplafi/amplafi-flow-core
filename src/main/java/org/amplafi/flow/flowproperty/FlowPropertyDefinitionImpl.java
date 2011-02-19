1620e0a9ea9a373c6c622649451e8f7fb5b63050
aectann <aectann@aelinux.site>
2011-02-19 08:59:35 +0800

    Removed unnecessary check.

--- src/main/java/org/amplafi/flow/flowproperty/FlowPropertyDefinitionImpl.java
+++ src/main/java/org/amplafi/flow/flowproperty/FlowPropertyDefinitionImpl.java
@@ -915,9 +915,7 @@ public class FlowPropertyDefinitionImpl extends AbstractFlowPropertyDefinitionPr
 
     public FlowPropertyDefinitionImpl initFlowPropertyValueChangeListener(FlowPropertyValueChangeListener flowPropertyValueChangeListener) {
         FlowPropertyDefinitionImpl flowPropertyDefinition = this.cloneIfTemplateAndNeedToAdd(this.flowPropertyValueChangeListeners, flowPropertyValueChangeListener);
-        if (this != flowPropertyDefinition || !templateFlowPropertyDefinition) {
-            flowPropertyDefinition.flowPropertyValueChangeListeners.add(flowPropertyValueChangeListener);
-        }
+        flowPropertyDefinition.flowPropertyValueChangeListeners.add(flowPropertyValueChangeListener);
         return flowPropertyDefinition;
     }
     public FlowPropertyDefinitionImpl addFlowPropertyValueChangeListeners(Collection<FlowPropertyValueChangeListener> additionalFlowPropertyValueChangeListeners) {
