739339b6b8167cf3f19adeebe64e726c369f8981
Patrick Moore <patmoore@amplafi.com>
2012-04-29 18:38:21 +0700

    capitalize the property name for use when creating the flow name for single property flows

--- src/main/java/org/amplafi/flow/definitions/FlowFromFlowPropertyDefinitionDefinitionSource.java
+++ src/main/java/org/amplafi/flow/definitions/FlowFromFlowPropertyDefinitionDefinitionSource.java
@@ -1,6 +1,7 @@
 package org.amplafi.flow.definitions;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -18,6 +19,7 @@ import org.amplafi.flow.flowproperty.FlowPropertyExpectationImpl;
 import org.amplafi.flow.flowproperty.PropertyUsage;
 import org.amplafi.flow.impl.FlowActivityImpl;
 import org.amplafi.flow.impl.FlowImpl;
+import org.apache.commons.lang.StringUtils;
 
 import com.sworddance.util.NotNullIterator;
 
@@ -27,6 +29,8 @@ import static org.amplafi.flow.flowproperty.PropertyUsage.internalState;
 
 /**
  * Define flows only needed to provide a property that is used by the ui of another flow. but is not needed by the execution of the flow itself.
+ *
+ * TODO: looks like we should be able to refactor some of these methods that create the flow. Bothered by the lack of DRY.
  * @author patmoore
  *
  */
@@ -40,9 +44,10 @@ public class FlowFromFlowPropertyDefinitionDefinitionSource implements Definitio
     public FlowFromFlowPropertyDefinitionDefinitionSource(FlowPropertyDefinitionImplementor...flowPropertyDefinitionImplementors) {
         for(FlowPropertyDefinitionImplementor flowPropertyDefinitionImplementor: NotNullIterator.<FlowPropertyDefinitionImplementor>newNotNullIterator(flowPropertyDefinitionImplementors)) {
             String flowPropertyName = flowPropertyDefinitionImplementor.getName();
-            FlowImpl flow = new FlowImpl(flowPropertyName+"Flow");
+            String capitalizedFlowPropertyName = StringUtils.capitalize(flowPropertyName);
+            FlowImpl flow = new FlowImpl(capitalizedFlowPropertyName+"Flow");
             flow.addPropertyDefinition(new FlowPropertyDefinitionImpl(FSSINGLE_PROPERTY_NAME).initAccess(flowLocal, internalState).initDefaultObject(flowPropertyName));
-            FlowActivityImpl flowActivity = new FlowActivityImpl(flowPropertyName+"FlowActivity");
+            FlowActivityImpl flowActivity = new FlowActivityImpl(capitalizedFlowPropertyName+"FlowActivity");
             flowActivity.addPropertyDefinition(flowPropertyDefinitionImplementor);
             flow.addActivity(flowActivity);
             put(this.flows, flow.getFlowPropertyProviderFullName(), flow);
@@ -50,10 +55,11 @@ public class FlowFromFlowPropertyDefinitionDefinitionSource implements Definitio
     }
 
     // HACK having to pass in the property name seems weak.
-    public void add(String flowPropertyName, FlowPropertyDefinitionProvider flowPropertyDefinitionProvider) {
-        FlowImpl flow = new FlowImpl(flowPropertyName+"Flow");
-        FlowActivityImpl flowActivity = new FlowActivityImpl("FA1");
-        flowPropertyDefinitionProvider.defineFlowPropertyDefinitions(flowActivity);
+    public void add(String flowPropertyName, FlowPropertyDefinitionProvider flowPropertyDefinitionProvider, List<FlowPropertyExpectation>additionalConfigurationParameters) {
+        String capitalizedFlowPropertyName = StringUtils.capitalize(flowPropertyName);
+        FlowImpl flow = new FlowImpl(capitalizedFlowPropertyName+"Flow");
+        FlowActivityImpl flowActivity = new FlowActivityImpl(capitalizedFlowPropertyName+"FlowActivity");
+        flowPropertyDefinitionProvider.defineFlowPropertyDefinitions(flowActivity, additionalConfigurationParameters);
         flow.addActivity(flowActivity);
         put(this.flows, flow.getFlowPropertyProviderFullName(), flow);
     }
@@ -68,9 +74,10 @@ public class FlowFromFlowPropertyDefinitionDefinitionSource implements Definitio
     public void add(FlowPropertyDefinitionProvider... flowPropertyDefinitionProviders) {
         for(FlowPropertyDefinitionProvider flowPropertyDefinitionProvider :flowPropertyDefinitionProviders) {
             for(String flowPropertyName : flowPropertyDefinitionProvider.getOutputFlowPropertyDefinitionNames()) {
-                FlowImpl flow = new FlowImpl(flowPropertyName+"Flow");
+                String capitalizedFlowPropertyName = StringUtils.capitalize(flowPropertyName);
+                FlowImpl flow = new FlowImpl(capitalizedFlowPropertyName+"Flow");
                 flow.addPropertyDefinition(new FlowPropertyDefinitionImpl(FlowConstants.FSSINGLE_PROPERTY_NAME).initAccess(flowLocal, internalState).initDefaultObject(flowPropertyName));
-                FlowActivityImpl flowActivity = new FlowActivityImpl("FA1");
+                FlowActivityImpl flowActivity = new FlowActivityImpl(capitalizedFlowPropertyName+"FlowActivity");
                 flowPropertyDefinitionProvider.defineFlowPropertyDefinitions(flowActivity, Arrays.<FlowPropertyExpectation>asList(new FlowPropertyExpectationImpl(FlowActivityPhase.finish)));
                 flow.addActivity(flowActivity);
                 put(this.flows, flow.getFlowPropertyProviderFullName(), flow);
