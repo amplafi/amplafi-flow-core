868ebccab8417eb096e8eb5e24dc83f9d87bad33
Patrick Moore <patmoore@amplafi.com>
2012-06-02 21:44:11 -0700

    correct hierarchy

--- src/main/java/org/amplafi/flow/flowproperty/FlowPropertySerializationNotPossibleException.java
+++ src/main/java/org/amplafi/flow/flowproperty/FlowPropertySerializationNotPossibleException.java
@@ -1,16 +1,24 @@
 package org.amplafi.flow.flowproperty;
 
-import com.sworddance.util.ApplicationGeneralException;
+import org.amplafi.flow.FlowException;
+import org.amplafi.flow.FlowState;
 
 /**
  * Should be thrown when a flow property serialization is impossible for any reason.
- * 
+ *
  * @author aectann@gmail.com (Konstantin Burov)
  *
  */
-public class FlowPropertySerializationNotPossibleException extends
-		ApplicationGeneralException {
+public class FlowPropertySerializationNotPossibleException extends FlowException {
 
-	private static final long serialVersionUID = 665430229497830087L;
+    private static final long serialVersionUID = 665430229497830087L;
+
+    public FlowPropertySerializationNotPossibleException(FlowState flowState, String message) {
+        super(flowState, message);
+    }
+
+    public FlowPropertySerializationNotPossibleException(FlowState flowState) {
+        super(flowState);
+    }
 
 }
