5f73a352a2b6a587264f639ea6595bf6abd1ec43
Patrick Moore <patmoore@farreach.es>
2017-12-25 01:06:07 -0800

    SerializationReader.java

--- src/main/java/org/amplafi/flow/translator/SerializationReader.java
+++ src/main/java/org/amplafi/flow/translator/SerializationReader.java
@@ -1,10 +1,14 @@
 package org.amplafi.flow.translator;
 
+import org.amplafi.flow.FlowTranslatorResolver;
+
 /**
- * Used to help deserialize.
+ * Used to deserialize a request
  * @author patmoore
  *
  */
 public interface SerializationReader extends FlowRendererProvider {
 
+    void setFlowRendererProvider(FlowRendererProvider flowRendererProvider);
+    void setFlowTranslatorResolver(FlowTranslatorResolver flowTranslatorResolver);
 }
